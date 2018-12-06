e398ed9dd9c2077ad528579daf62c9cec689219a
mubarakshin_dr <mubarakshin-1998@mail.ru>
2018-12-07 00:18:35 +0300

    Real class added

--- src/main/java/ru/innopolis/comicon/stdlib/Boolean.java
+++ src/main/java/ru/innopolis/comicon/stdlib/Boolean.java
@@ -1,35 +1,4 @@
 package ru.innopolis.comicon.stdlib;
 
 public class Boolean {
-    private boolean value;
-
-    //Constructors
-    public Boolean(Boolean p) {
-        this.value = p.value;
-    }
-    Boolean(boolean p) {
-        this.value = p;
-    }
-
-    //Conversion
-    public Integer toInteger(){
-        return new Integer(this.value ? 1 : 0);
-    }
-
-    //Boolean operators
-    public Boolean Or(Boolean p){
-        return new Boolean(this.value || p.value);
-    }
-
-    public Boolean And(Boolean p){
-        return new Boolean(this.value && p.value);
-    }
-
-    public Boolean Xor(Boolean p){
-        return new Boolean(this.value ^ p.value);
-    }
-
-    public Boolean Not(){
-        return new Boolean(!this.value);
-    }
 }
--- src/main/java/ru/innopolis/comicon/stdlib/Real.java
+++ src/main/java/ru/innopolis/comicon/stdlib/Real.java
@@ -1,4 +1,118 @@
 package ru.innopolis.comicon.stdlib;
 
+import java.lang.Math;
+
 public class Real {
+    public double value;
+
+    // Constructors
+    private Real(double temp) {
+        this.value = temp;
+    }
+
+    private Real(Integer temp) {
+        this.value = temp.toReal();
+    }
+
+    private Real(Real temp) {
+        this.value = temp.value;
+    }
+
+    // Features
+    public Real Max() {
+        return new Real(Double.MAX_VALUE);
+    }
+
+    public Real Min() {
+        return new Real(Double.MIN_VALUE);
+    }
+
+    public Real Epsilon() {
+        return new Real(Math.ulp(1));
+    }
+
+    public Integer toInteger() {
+        return new Integer(int(this.value));
+    }
+
+    public Real UnaryMinus() {
+        return new Real(this.value - 1);
+    }
+
+    public Real Plus(Real temp) {
+        return new Real(this.value + temp.value);
+    }
+
+    public Real Plus(Integer temp) {
+        return new Real(this.value + temp.value);
+    }
+
+    public Real Minus(Real temp) {
+        return new Real(this.value - temp.value);
+    }
+
+    public Real Minus(Integer temp) {
+        return new Real(this.value - temp.toReal().value);
+    }
+
+    public Real Mult(Real temp) {
+        return new Real(this.value * temp.value);
+    }
+
+    public Real Mult(Integer temp) {
+        this.value * temp.toReal().value;
+    }
+
+    public Real Div(Real temp) {
+        return new Real(this.value / temp.value);
+    }
+
+    public Real Div(Integer temp) {
+        return new Real(this.value / temp.toReal().value);
+    }
+
+    public Real Rem(Integer temp) {
+        return new Real(this.value % temp.value);
+    }
+
+    // Relations
+    public Boolean Less(Real temp) {
+        return new Boolean(this.value < temp.value);
+    }
+
+    public Boolean Less(Integer temp) {
+        return new Boolean(this.value < temp.value);
+    }
+
+    public Boolean LessEqual(Real temp) {
+        return new Boolean(this.value <= temp.value);
+    }
+
+    public Boolean LessEqual(Integer temp) {
+        return new Boolean(this.value <= temp.value);
+    }
+
+    public Boolean Greater(Real temp) {
+        return new Boolean(this.value > temp.value);
+    }
+
+    public Boolean Greater(Integer temp) {
+        return new Boolean(this.value > temp.value);
+    }
+
+    public Boolean GreaterEqual(Real temp) {
+        return new Boolean(this.value >= temp.value);
+    }
+
+    public Boolean GreaterEqual(Integer temp) {
+        return new Boolean(this.value >= temp.value);
+    }
+
+    public Boolean Equal(Real temp) {
+        return new Boolean(this.value == temp.value);
+    }
+
+    public Boolean Equal(Integer temp) {
+        return new Boolean(this.value == temp.toReal().value);
+    }
 }
